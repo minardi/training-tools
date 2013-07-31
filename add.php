7f8196e40ffcebde566433e92110293dc9c4f279
unknown <msheltc@DPCLASS06.training.local>
2013-07-31 11:24:33 +0300

    demo

--- add.php
+++ add.php
@@ -1,13 +1,10 @@
 <?
   if($_REQUEST["operand1"] && $_REQUEST["operand2"])
-  {
-    $op1 = $_REQUEST["operand1"];
-    $op2 = $_REQUEST["operand2"];
-
-    echo $op1 + $op2;
+  {       
+    echo $_REQUEST["operand1"] + $_REQUEST["operand2"]; 
   }
   else
   {
-    echo "Bad operands format!";
+    echo "Bad operands format!";	
   }
 ?>
