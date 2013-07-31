5dc96aa31760dc89c5d0de5f612f5ac203fe5105
unknown <msheltc@DPCLASS06.training.local>
2013-07-31 11:32:21 +0300

    demo2

--- add.php
+++ add.php
@@ -1,7 +1,10 @@
 <?
   if($_REQUEST["operand1"] && $_REQUEST["operand2"])
   {       
-    echo $_REQUEST["operand1"] + $_REQUEST["operand2"]; 
+    $op1 = $_REQUEST["operand1"];
+    $op2 = $_REQUEST["operand2"];
+
+    echo $op1 + $op2;
   }
   else
   {
